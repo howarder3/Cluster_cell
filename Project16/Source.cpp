22fd4d89c24918fc554da12355e6fa9a2d920941
Howard Weng <howarder3@gmail.com>
2017-05-25 05:35:39 +0800

    hw2-1 12cell finish

--- Project16/Source.cpp
+++ Project16/Source.cpp
@@ -95,8 +95,18 @@ int main()
 	cell10.print_result();
 	cell11.print_result();
 	cell12.print_result();
-	cout << "\nAverage Blocking probability = " << (cell1.blocking_probability + cell2.blocking_probability + cell3.blocking_probability + cell4.blocking_probability + cell5.blocking_probability + cell6.blocking_probability
-		+ cell7.blocking_probability + cell8.blocking_probability + cell9.blocking_probability + cell10.blocking_probability + cell11.blocking_probability + cell12.blocking_probability) / 12 <<endl;
+	cout << "\nTotal call : " << cell1.call_count + cell2.call_count + cell3.call_count
+		+ cell4.call_count + cell5.call_count + cell6.call_count
+		+ cell7.call_count + cell8.call_count + cell9.call_count
+		+ cell10.call_count + cell11.call_count + cell12.call_count;
+	cout << " Blocked call : " << cell1.blockcall_count + cell2.blockcall_count + cell3.blockcall_count
+		+ cell4.blockcall_count + cell5.blockcall_count + cell6.blockcall_count
+		 +cell7.blockcall_count + cell8.blockcall_count + cell9.blockcall_count
+		 +cell10.blockcall_count + cell11.blockcall_count + cell12.blockcall_count<< endl;
+	cout << "Average Blocking probability = " << (cell1.blocking_probability + cell2.blocking_probability + cell3.blocking_probability
+	+ cell4.blocking_probability + cell5.blocking_probability + cell6.blocking_probability
+		+ cell7.blocking_probability + cell8.blocking_probability + cell9.blocking_probability 
+		+ cell10.blocking_probability + cell11.blocking_probability + cell12.blocking_probability)	/ 12 << endl;
 
 	//out_file.close();   //ÃöÀÉ
 	system("pause");
@@ -104,13 +114,12 @@ int main()
 }
 
 
-
 cell:: cell(int i)
 {
 	cell_name = i;
 	arrival_time[0] = 0;
-	mean_poisson = ((i%4) + 1) * 1;
-	mean_exponential = ((i%4) + 1)*0.2; //per time the event happened
+	mean_poisson = ((i%4) + 1) * 1.0;
+	mean_exponential = ((i%4) + 1)*0.2; 
 }
 
 void cell::process()
@@ -122,7 +131,7 @@ void cell::process()
 		if (arrival_time[i] > 100)
 			break;
 		call_count++;
-		service_time[i] = 1 / (Exponential(mean_exponential)); //per time the event happened -> the event happen time
+		service_time[i] = (Exponential(mean_exponential)); //per time the event happened -> the event happen time
 		 //cout << inter_arrival_time << "\t\t\t" << c1.arrival_time[i]<<"\t\t"  << Exponential(mean_exponential) << endl;
 		cout << "Call " << call_count << " arrival: arrival time = " << arrival_time[i] << "\tservice time = " << service_time[i] << endl;
 		if (arrival_time[i] > ch1_expired_time)
@@ -141,42 +150,42 @@ void cell::process()
 		if (ch1 == 0)
 		{
 			ch1_expired_time = arrival_time[i] + service_time[i];
-			cout << "Call success, used ch1, the call will end at :" << ch1_expired_time << endl;
+			cout << "Call success, used ch" << 1 + ((cell_name - 1) % 3) * 5 << ", the call will end at :" << ch1_expired_time << endl;
 			ch1 = 1;
 		}
 		else if (ch2 == 0)
 		{
 			ch2_expired_time = arrival_time[i] + service_time[i];
-			cout << "Call success, used ch2, the call will end at :" << ch2_expired_time << endl;
+			cout << "Call success, used ch" << 2 + ((cell_name - 1) % 3) * 5 << ", the call will end at :" << ch2_expired_time << endl;
 			ch2 = 1;
 		}
 		else if (ch3 == 0)
 		{
 			ch3_expired_time = arrival_time[i] + service_time[i];
-			cout << "Call success, used ch3, the call will end at :" << ch3_expired_time << endl;
+			cout << "Call success, used ch" << 3 + ((cell_name - 1) % 3) * 5 << ", the call will end at :" << ch3_expired_time << endl;
 			ch3 = 1;
 		}
 		else if (ch4 == 0)
 		{
 			ch4_expired_time = arrival_time[i] + service_time[i];
-			cout << "Call success, used ch4, the call will end at :" << ch4_expired_time << endl;
+			cout << "Call success, used ch" << 4 + ((cell_name - 1) % 3) * 5 << ", the call will end at :" << ch4_expired_time << endl;
 			ch4 = 1;
 		}
 		else if (ch5 == 0)
 		{
 			ch5_expired_time = arrival_time[i] + service_time[i];
-			cout << "Call success, used ch5, the call will end at :" << ch5_expired_time << endl;
+			cout << "Call success, used ch" << 5 + ((cell_name - 1) % 3) * 5 << ", the call will end at :" << ch5_expired_time << endl;
 			ch5 = 1;
 		}
 		else
 		{
 			blockcall_count++;
 			cout << "All channel are using, call failed." << endl;
-			cout << "ch1 will end call at:" << ch1_expired_time << endl;
-			cout << "ch2 will end call at:" << ch2_expired_time << endl;
-			cout << "ch3 will end call at:" << ch3_expired_time << endl;
-			cout << "ch4 will end call at:" << ch4_expired_time << endl;
-			cout << "ch5 will end call at:" << ch5_expired_time << endl;
+			cout << "ch"<<1+ ((cell_name - 1) % 3) * 5 <<" will end call at:" << ch1_expired_time << endl;
+			cout << "ch"<<2+ ((cell_name - 1) % 3) * 5 <<" will end call at:" << ch2_expired_time << endl;
+			cout << "ch" << 3 + ((cell_name - 1) % 3) * 5 << " will end call at:" << ch3_expired_time << endl;
+			cout << "ch" << 4 + ((cell_name - 1) % 3) * 5 << " will end call at:" << ch4_expired_time << endl;
+			cout << "ch" << 5 + ((cell_name-1) % 3)*5 << " will end call at:" << ch5_expired_time << endl;
 			cout << "current blocked call = " << blockcall_count << endl;
 		}
 		//out_file << Poisson(mean)+0.5 <<"\t\t\t"<< Exponential(mean) << endl;
@@ -186,13 +195,13 @@ void cell::process()
 
 void cell::print_result()
 {
-	cout << "* Cell "<<cell_name<<" * " << endl;
+	cout << "* BS "<<cell_name<<" * " << endl;
 	cout << "Total call : " << call_count << " Blocked call : " << blockcall_count << endl;
 	blocking_probability = blockcall_count*1.0 / call_count;
 	cout << "Blocking probability = " << blocking_probability << endl;
 }
 
-int Poisson(double mean) //Special technique required: Box-Muller method...
+int Poisson(double mean) 
 {
 	double R;
 	double sum = 0;
@@ -203,7 +212,7 @@ int Poisson(double mean) //Special technique required: Box-Muller method...
 	while (sum <= mean)
 	{
 
-		R = (float)rand() / (float)(RAND_MAX + 1);
+		R = (double)rand() / (double)(RAND_MAX + 1);
 		z = -log(R);
 		sum += z;
 		i++;
@@ -213,11 +222,11 @@ int Poisson(double mean) //Special technique required: Box-Muller method...
 
 double Exponential(double mean)
 {
-	//Generate a random number between 0 and 1.
-	//REMEMBER: Always cast the oparands of a division to float, or truncation will be performed.
-	double R;
-	R = (float)rand() / (float)(RAND_MAX + 1);
+	double p;
+	p = (double)rand() / (double)(RAND_MAX + 1);
+	double temp = log(1 - p);
+	temp = -(temp) / (mean);
 
-	//Put it as a parameter to the inverse distribution function.
-	return  -mean*log(R);
+
+	return temp;
 }
